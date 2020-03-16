c3fa0fe02df99f58d8ac551844cbfea19a1e0573
JonCas22 <joncaso22@gmail.com>
2020-03-16 17:26:52 +0100

    Agregar archivos de proyecto.

---
+++ ApiRest_Personas/Controllers/PersonasController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prueba.Models;
+
+namespace ApiRest_Personas.Controllers
+{
+    [Route("/personas")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly masterContext _context;
+
+        public PersonasController(masterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Personas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personas>>> GetPersonas()
+        {
+            return await _context.Personas.ToListAsync();
+        }
+
+        // GET: api/Personas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personas>> GetPersonas(long id)
+        {
+            var personas = await _context.Personas.FindAsync(id);
+
+            if (personas == null)
+            {
+                return NotFound();
+            }
+
+            return personas;
+        }
+
+        // PUT: api/Personas/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut]
+        public async Task<ActionResult<IEnumerable<Personas>>> PutPersonas(Personas personas)
+        {
+            if (personas==null)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(personas).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return await _context.Personas.ToListAsync();
+        }
+
+        // POST: api/Personas
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Personas>>> PostPersonas(Personas personas)
+        {
+            _context.Personas.Add(personas);
+            await _context.SaveChangesAsync();
+
+            return await _context.Personas.ToListAsync();
+        }
+
+        // DELETE: api/Personas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<IEnumerable<Personas>>> DeletePersonas(long id)
+        {
+            var personas = await _context.Personas.FindAsync(id);
+            if (personas == null)
+            {
+                return NotFound();
+            }
+
+            _context.Personas.Remove(personas);
+            await _context.SaveChangesAsync();
+
+            return await _context.Personas.ToListAsync();
+        }
+
+        private bool PersonasExists(long id)
+        {
+            return _context.Personas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ApiRest_Personas/Models/masterContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Prueba.Models
+{
+    public partial class masterContext : DbContext
+    {
+        public masterContext()
+        {
+        }
+
+        public masterContext(DbContextOptions<masterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Personas> Personas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personas>(entity =>
+            {
+                entity.ToTable("personas");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasColumnName("city")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone).HasColumnName("phone");
+                entity.Property(e => e.Province).HasColumnName("province")
+                    .IsRequired()
+                    .HasColumnName("province")
+                    .HasMaxLength(100)
+                    .IsUnicode(false); ;
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
